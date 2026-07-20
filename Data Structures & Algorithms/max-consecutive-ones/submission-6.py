1d2fa8c53fa61f616c370728e3a10f701ed8adbe
Mohamed Islem Ayari <116555936+mofach90@users.noreply.github.com>
2026-07-20 18:36:32 +0200

    Add: max-consecutive-ones - submission-6

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-6.py
@@ -0,0 +1,92 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maxi = 0
+        i = 0
+        curr = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                curr += 1
+            else:
+                curr = 0
+            maxi = max(maxi, curr)
+        return maxi
+            
+
+
+
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         maxi = 0
+#         i = 0
+#         j = 0
+#         while i < len(nums):
+#             if nums[i]:
+#                 j = i
+#                 print(f"i {i}, j {j}")
+#                 while nums[j]:
+#                     j += 1
+#                     print(j)
+#                     print("nums[j]", j)
+#                     if j == len(nums):
+#                         break
+#                 maxi = max(maxi, j - i)
+#                 i = j
+#             i += 1
+
+#         print(maxi)
+#         return maxi 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         max_num = 0
+#         count = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 count += 1
+#                 max_num = max(max_num, count)
+#                 print(max_num)
+#             else:
+#                 count = 0
+#         return max_num    
+
+
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         maxi = 0
+#         len = 0
+#         for i in nums:
+#             if i == 1:
+#                 len += 1
+#             else:
+#                 len = 0
+#             maxi = max(maxi, len) 
+#         return maxi
+            
+        
