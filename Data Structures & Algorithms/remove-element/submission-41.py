02ab9f52ab66dae8d9ca1341b99be32910fd608a
Mohamed Islem Ayari <116555936+mofach90@users.noreply.github.com>
2026-07-22 08:12:40 +0200

    Add: remove-element - submission-41

---
+++ Data Structures & Algorithms/remove-element/submission-41.py
@@ -0,0 +1,125 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        l = len(nums)
+        p = 0
+        for i in range(l):
+            if nums[i] != val:
+                nums[p] = nums[i]
+                p += 1
+
+
+        return p
+            
+
+                
+
+
+
+
+
+# # brute force
+# class Solution:
+#     def removeElement(self, nums: list[int], val: int) -> int:
+#         l = len(nums)
+
+#         for n in range(l):
+#             for i in range(l-1):
+#                 if nums[i] == val:
+#                     nums[i],nums[i+1] = nums[i+1], nums[i]
+#         for num in nums:
+#             if num == val:
+#                 l -= 1
+#         return l
+
+
+
+
+# class Solution:
+#     def removeElement(self, nums: list[int], val: int) -> int:
+#         k = 0
+#         for i in range(len(nums)):
+#             if nums[i] != val:
+#                 print("k = ",k)
+#                 print(f"num {i} before =",nums)
+#                 nums[k] = nums[i]
+#                 print(f"num {i} after =",nums)
+#                 k += 1
+#         return k
+# class Solution:
+#     def removeElement(self, nums: list[int], val: int) -> int:
+#         pointer1=-1
+#         m = 0
+#         k=0
+#         for i in range(len(nums)):
+#             if nums[i] == val:
+#                 if pointer1 == -1:
+#                     pointer1 =i
+#                 for j in range( pointer1, len(nums)):
+#                     if nums[j] != val:
+#                         pointer1 = j+1
+#                         nums[i],nums[j]=nums[j],nums[i]
+#                         m +=1
+#                         break
+#                     if pointer1 == len(nums):
+#                         break
+#             if nums[i] != val:
+#                 k += 1
+#         return k
+
+
+
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = 0
+#         j=0
+#         k=0
+#         pointer1 = 0
+#         l = len(nums)
+#         for i in range(len(nums)):
+#             while nums[i] == val:
+#                 pointer1 += 1
+#                 # print("pointer1=",pointer1) 
+#                 # print(f"num {i} before =",nums) 
+#                 nums[i] = "_"
+#                 # print(f"num {i} after =",nums) 
+#                 nums[i],nums[l-pointer1] = nums[l-pointer1],nums[i]
+#                 # print(f"num {i} final =",nums) 
+                
+#         k = len(nums) - pointer1
+#         # print("k=",k)
+#         return k
+
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = 0
+#         j=0
+#         k=0
+#         pointer1 = 0
+#         for i in range(len(nums)):
+#             while nums[i] == val:
+#                 pointer1 += 1
+#                 print("pointer1=",pointer1) 
+#                 print(f"num {i} before =",nums) 
+#                 nums[i] = "_"
+#                 print(f"num {i} after =",nums) 
+#                 for j in range(i, (len(nums)-1)):
+#                     nums[j],nums[j+1] = nums[j+1],nums[j]
+#                 print(f"num {i} final =",nums) 
+                
+#         k = len(nums) - pointer1
+#         print("k=",k)
+#         return k
+
+
+
+
+            
+
+
+
+
+
+# nums --> array of int
+# val intege
+# remove all val from nums = Override with "_"
+# return how much numbers are not equal to val in muns --> k
